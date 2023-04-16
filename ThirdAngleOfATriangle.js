fa49e1eefdaf0b55bc8d87b66c06846803f18f84
Rajesh-Reel <rajreel2@hotmail.com>
2023-04-16 11:29:22 +0100

    added third angle of a triangle 8 kyu solution

---
+++ ThirdAngleOfATriangle.js
@@ -0,0 +1,20 @@
+/*
+
+8 kyu
+
+Third Angle of a Triangle
+
+You are given two interior angles (in degrees) of a triangle.
+
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested.
+
+https://en.wikipedia.org/wiki/Triangle
+
+*/
+
+function otherAngle(a, b) {
+    let third = 180 - a - b;
+    return third;
+}
