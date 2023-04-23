619c347aa7ab9c9d0d64de94eddf5fc59d92fb6f
Rajesh-Reel <rajreel2@hotmail.com>
2023-04-23 12:24:59 +0100

    added calculate bmi 8 kyu solution

---
+++ CalculateBMI.js
@@ -0,0 +1,32 @@
+/*
+
+8 kyu
+
+Calculate BMI 
+
+Write function bmi that calculates body mass index (bmi = weight / height2).
+
+if bmi <= 18.5 return "Underweight"
+
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese"
+
+*/
+
+function bmi(weight, height) {
+    let hxh = height * height;
+    let newBmi = weight / hxh;
+    
+    if(newBmi <= 18.5){
+      return "Underweight";
+    } else if (newBmi <= 25){
+      return "Normal";
+    } else if (newBmi <= 30){
+      return "Overweight";
+    } else {
+    return "Obese";
+    }
+}
