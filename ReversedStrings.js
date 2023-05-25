2f0e77ade12122d1d2bd63770818e846c6c15db4
Rajesh-Reel <rajreel2@hotmail.com>
2023-05-25 16:59:45 +0100

    added reversed strings 8 kyu solution

---
+++ ReversedStrings.js
@@ -0,0 +1,16 @@
+/*
+
+8 kyu 
+
+Reversed Strings 
+
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+
+*/
+
+function solution(str){
+    return [...str].reverse().join("");
+}
