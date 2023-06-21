0e62c011fd1695c938ead174edb0cba23e99881e
Rajesh-Reel <rajreel2@hotmail.com>
2023-06-21 09:32:55 +0100

    added If you can't sleep, just count sheep 8 kyu solution

---
+++ IfYouCan'tSleep,JustCountSheep!!.js
@@ -0,0 +1,19 @@
+/*
+
+8 kyu 
+
+If you can't sleep, just count sheep!!
+
+Task:
+
+Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+*/
+
+var countSheep = function (num){
+    let str = "";
+    for (let i = 1; i <= num; i++) {
+      str += i.toString() + ` sheep...`;
+    }
+    return str;
+}
