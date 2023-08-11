3f97da1d40a2a47f54eb7c53e361b98285c1e136
Rajesh-Reel <rajreel2@hotmail.com>
2023-08-11 13:45:57 +0100

    added super duper easy 8 kyu solution

---
+++ SuperDuperEasy.js
@@ -0,0 +1,16 @@
+/*
+
+8 kyu 
+
+Super Duper Easy
+
+Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+*/
+
+function problem(x){
+    if (typeof x === "string") {
+      return "Error";
+    }
+    return x * 50 + 6;
+}
