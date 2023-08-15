a26d734e1ee08b33f7dcad0855fc72804b01b62e
Rajesh-Reel <rajreel2@hotmail.com>
2023-08-15 10:16:33 +0100

    added calculate average 8 kyu solution

---
+++ CalculateAverage.js
@@ -0,0 +1,19 @@
+/*
+
+8 kyu 
+
+Calculate average 
+
+Write a function which calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0.
+
+*/
+
+function findAverage(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    return sum / array.length || sum;
+}
