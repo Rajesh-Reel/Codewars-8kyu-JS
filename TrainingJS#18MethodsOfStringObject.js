071ef6ed9d0e099c7938c9c4bfcedd198496ffff
Rajesh-Reel <rajreel2@hotmail.com>
2024-08-29 12:34:54 +0100

    added training js #18 methods of string object 8 kyu solution

---
+++ TrainingJS#18MethodsOfStringObject.js
@@ -0,0 +1,28 @@
+/*
+
+8 kyu 
+
+Training JS #18: Methods of String Object 
+
+Task
+Implement a function which accepts 2 arguments: string and separator.
+
+The expected algorithm: split the string into words by spaces, split each word into separate characters and join them back with the specified separator, join all the resulting "words" back into a sentence with spaces.
+
+For example:
+
+splitAndMerge("My name is John", " ")  ==  "M y n a m e i s J o h n"
+splitAndMerge("My name is John", "-")  ==  "M-y n-a-m-e i-s J-o-h-n"
+splitAndMerge("Hello World!", ".")     ==  "H.e.l.l.o W.o.r.l.d.!"
+splitAndMerge("Hello World!", ",")     ==  "H,e,l,l,o W,o,r,l,d,!"
+
+*/
+
+function splitAndMerge(string, separator) {
+    return string
+      .split(" ")
+      .map(words => words
+           .split("")
+           .join(separator))
+      .join(" ");
+}
